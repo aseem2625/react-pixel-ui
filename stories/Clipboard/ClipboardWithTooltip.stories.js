dfd1136675f9d1394ad6df597fd41bdfb269136d
aseemgupta <aseemgupta2625@gmail.com>
2019-06-10 02:15:34 +0530

    [Storybook]: Add ClipboardWithTooltip

---
+++ stories/Clipboard/ClipboardWithTooltip.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { text } from '@storybook/addon-knobs';
+
+import { ClipboardWithTooltip, Icon } from '../../dist/index.min';
+
+
+storiesOf('Components/Clipboard', module)
+  .add(
+    'ClipboardWithTooltip',
+    () => (
+      <ClipboardWithTooltip
+        toCopy={text('To copy', 'Some Text for ClipboardWithTooltip')}
+        tooltipCopyText={text('Tooltip text before copy', 'Copy some url')}
+        tooltipCopiedText={text('Tooltip text on copy', 'Copied')}
+      >
+        Copy Icon
+      </ClipboardWithTooltip>
+    )
+  );
