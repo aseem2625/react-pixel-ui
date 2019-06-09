c30a5c587086a4eee1c79a491209bcd2add7e821
aseemgupta <aseemgupta2625@gmail.com>
2019-06-10 02:47:39 +0530

    [Storybook]: Add RipplePointer

---
+++ stories/RipplePointer.stories.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { boolean, text } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+
+import { RipplePointer } from '../dist/index.min';
+
+
+storiesOf('Components', module)
+  .add(
+    'RipplePointer',
+    () => {
+      const isPrimary = boolean('Primary Ripple', false),
+      className = text('Classes', 'class1 class2');
+
+      return (
+        <div style={{ height: 100, width: 100, background: `${isPrimary ? '#fff' : '#000'}` }}>
+          <RipplePointer
+            isPrimary={isPrimary}
+            className={className}
+            onClick={action('RipplePointer clicked!')}
+            style={{ left: 50, top: 50 }}
+          />
+        </div>
+      );
+    }
+  );
