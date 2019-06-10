9408e163a0d329d7841e6dd09c81a1395453a331
aseemgupta <aseemgupta2625@gmail.com>
2019-06-10 23:31:30 +0530

    [Storybook]: Add EscPressLayer

---
+++ stories/xtra/EscPressLayer.stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { boolean } from '@storybook/addon-knobs';
+
+import { EscPressLayer, Button } from '../../dist/index.min';
+
+
+storiesOf('Components/Utilities', module)
+  .add(
+    'EscPressLayer',
+    () => (
+      <EscPressLayer
+        enabled={boolean('Enabled', true)}
+        onEscPress={_ => alert('Pressed Escape')}
+      >
+        <Button>Some Button</Button>
+      </EscPressLayer>
+    )
+  );
