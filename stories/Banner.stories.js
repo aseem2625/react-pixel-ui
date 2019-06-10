d7212d87d5f9818a0d5dba8529f8bb36c93e61b5
aseemgupta <aseemgupta2625@gmail.com>
2019-06-10 12:25:33 +0530

    [Storybook]: Add Banner

---
+++ stories/Banner.stories.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { text, select } from '@storybook/addon-knobs';
+
+import { Banner } from '../dist/index.min';
+
+
+storiesOf('Components', module)
+  .add(
+    'Banner',
+    () => {
+      return (
+        <Banner
+          className={text('Classes', 'class1 class2')}
+          type={select('Type', ['', 'green', 'red', 'yellow'])}
+        >
+          Add some Banner text
+        </Banner>
+      );
+    }
+  );
