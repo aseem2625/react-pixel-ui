4c704da1fafb8210486b525b5482281f648eaee6
aseemgupta <aseemgupta2625@gmail.com>
2019-06-12 03:52:11 +0530

    [Storybook]: Add Dropdown

---
+++ stories/Dropdown.stories.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { text, boolean } from '@storybook/addon-knobs';
+
+import { Dropdown } from '../dist/index.min';
+
+
+storiesOf('Components', module)
+  .add(
+    'Dropdown ',
+    () => {
+      const showBeforeOptions = boolean('_Show before options', false),
+      showAfterOptions = boolean('_Show after options', false);
+
+      return (
+        <Dropdown
+          className={text('Classes', 'class1 class2')}
+          trigger={() => <div>Some trigger element</div>}
+          showOnHover={boolean('Show on hover', false)}
+          beforeOptions={showBeforeOptions && (close => <b>Before options element</b>)}
+          afterOptions={showAfterOptions && (close => <b>After options element</b>)}
+        >
+          {
+            (close) => (
+              <>
+                <div>div element</div>
+                <span onClick={close}> span element (click to close)</span>
+                <button>button element</button>
+              </>
+            )
+          }
+        </Dropdown>
+      );
+    }
+  );
+
