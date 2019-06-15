f1376710100cc3ae2cc27a7aec531f09212d9fba
aseemgupta <aseemgupta2625@gmail.com>
2019-06-15 16:34:16 +0530

    [Storybook]: Refactor Dropdown

--- stories/Dropdown.stories.js
+++ stories/Dropdown.stories.js
@@ -6,20 +6,61 @@ import { text, boolean } from '@storybook/addon-knobs';
 import { Dropdown } from 'components/index';
 
 
-storiesOf('Components', module)
+storiesOf('Components/Dropdown', module)
   .add(
-    'Dropdown ',
+    'simple',
     () => {
-      const showBeforeOptions = boolean('_Show before options', false),
-      showAfterOptions = boolean('_Show after options', false);
-
       return (
         <Dropdown
           className={text('Classes', 'class1 class2')}
           trigger={() => <div>Some trigger element</div>}
           showOnHover={boolean('Show on hover', false)}
-          beforeOptions={showBeforeOptions && (close => <b>Before options element</b>)}
-          afterOptions={showAfterOptions && (close => <b>After options element</b>)}
+        >
+          {
+            (close) => (
+              <>
+                <div>div element</div>
+                <span onClick={close}> span element (click to close)</span>
+                <button>button element</button>
+              </>
+            )
+          }
+        </Dropdown>
+      );
+    }
+  )
+  .add(
+    'with beforeOptions',
+    () => {
+      return (
+        <Dropdown
+          className={text('Classes', 'class1 class2')}
+          trigger={() => <div>Some trigger element</div>}
+          showOnHover={boolean('Show on hover', false)}
+          beforeOptions={(close => <b>Before options element</b>)}
+        >
+          {
+            (close) => (
+              <>
+                <div>div element</div>
+                <span onClick={close}> span element (click to close)</span>
+                <button>button element</button>
+              </>
+            )
+          }
+        </Dropdown>
+      );
+    }
+  )
+  .add(
+    'with afterOptions',
+    () => {
+      return (
+        <Dropdown
+          className={text('Classes', 'class1 class2')}
+          trigger={() => <div>Some trigger element</div>}
+          showOnHover={boolean('Show on hover', false)}
+          afterOptions={close => <b>After options element</b>}
         >
           {
             (close) => (
