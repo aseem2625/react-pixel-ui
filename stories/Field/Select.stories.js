e878dfa7376013e704af29dd335c40f26bbc302c
aseemgupta <aseemgupta2625@gmail.com>
2019-06-15 19:50:26 +0530

    [Storybook]: Add Select field

---
+++ stories/Field/Select.stories.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { text, boolean, object } from '@storybook/addon-knobs';
+
+import { Select, SelectElement } from 'components/index';
+
+
+const countries = [
+  {
+    name: 'Argentina',
+    code: 'ARG',
+    flag: 'https://restcountries.eu/data/arg.svg',
+    continent: 'South America',
+  },
+  {
+    name: 'Brazil',
+    code: 'BRA',
+    flag: 'https://restcountries.eu/data/bra.svg',
+    continent: 'South America',
+  },
+  {
+    name: 'Canada',
+    code: 'CAN',
+    flag: 'https://restcountries.eu/data/can.svg',
+    continent: 'North America',
+  },
+  {
+    name: 'China',
+    code: 'CHN',
+    flag: 'https://restcountries.eu/data/chn.svg',
+    continent: 'Asia',
+  }
+];
+
+
+storiesOf('Components/Field/Select', module)
+  .add(
+    'with FieldWrapper',
+    () => (
+      <Select
+        name={text('Name', 'field_name')}
+        label={text('Label', 'Select Element')}
+        placeholder={text('Placeholder', 'Enter value')}
+        description={text('Description', 'Some description for this input')}
+        defaultValue={object('Default value', countries[2])}
+        options={object('Options', countries)}
+        disabled={boolean('Disabled', false)}
+        show={boolean('Show', false)}
+      />
+    )
+  )
+  .add(
+    'without FieldWrapper',
+    () => (
+      <SelectElement
+        name={text('Name', 'field_name')}
+        placeholder={text('Placeholder', 'Enter value')}
+        defaultValue={object('Default value', countries[1])}
+        options={object('Options', countries)}
+        disabled={boolean('Disabled', false)}
+        show={boolean('Show', true)}
+      />
+    )
+  );
