3b82051eba54235faa26407b5b8aa5e3f65f4db1
aseemgupta <aseemgupta2625@gmail.com>
2019-06-15 22:11:29 +0530

    [Storybook]: Update Select field

--- stories/Field/Select.stories.js
+++ stories/Field/Select.stories.js
@@ -33,6 +33,42 @@ const countries = [
   }
 ];
 
+const nestedCountries = [
+  {
+    label: 'America',
+    options: [
+      {
+        name: 'Argentina',
+        code: 'ARG',
+        flag: 'https://restcountries.eu/data/arg.svg',
+        continent: 'South America',
+      },
+      {
+        name: 'Brazil',
+        code: 'BRA',
+        flag: 'https://restcountries.eu/data/bra.svg',
+        continent: 'South America',
+      },
+    ]
+  },   {
+    label: 'Asia',
+    options: [
+      {
+        name: 'India',
+        code: 'IND',
+        flag: 'https://restcountries.eu/data/ind.svg',
+        continent: 'Asia',
+      },
+      {
+        name: 'China',
+        code: 'CHN',
+        flag: 'https://restcountries.eu/data/chn.svg',
+        continent: 'Asia',
+      }
+    ]
+  }
+];
+
 
 storiesOf('Components/Field/Select', module)
   .add(
@@ -59,7 +95,7 @@ storiesOf('Components/Field/Select', module)
         name={text('Name', 'field_name')}
         placeholder={text('Placeholder', 'Enter value')}
         defaultValue={object('Default value', countries[1])}
-        options={object('Options', countries)}
+        options={object('Options', nestedCountries)}
         disabled={boolean('Disabled', false)}
         show={boolean('Show', true)}
         enableSearch={boolean('Enable search', true)}
