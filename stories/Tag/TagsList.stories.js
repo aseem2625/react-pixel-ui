adcf9861f85397608e160aaaadd4084323d7a3e0
aseemgupta <aseemgupta2625@gmail.com>
2019-06-22 01:10:18 +0530

    [Storybook]: Try dummy parent container in TagsList.

--- stories/Tag/TagsList.stories.js
+++ stories/Tag/TagsList.stories.js
@@ -10,7 +10,7 @@ storiesOf('Components/Tag', module)
   .add(
     'TagsList',
     () => (
-      <div style={{width: 250, border: '1px solid black'}}>
+      <div style={{width: 500, background: '#f9f9f9', padding: 32}}>
         <TagsList className={text('className', 'class1 class2')}>
           <Tag>Simple Tag </Tag>
           <Tag>Simple very big text Tag</Tag>
