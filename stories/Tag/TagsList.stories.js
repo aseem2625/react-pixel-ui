b2e5f639372e2cc5bbc9b2a71f3f6f31a200d0d0
aseemgupta <aseemgupta2625@gmail.com>
2019-06-22 03:08:14 +0530

    [Storybook]: Add DummyContainer as helper component

--- stories/Tag/TagsList.stories.js
+++ stories/Tag/TagsList.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { text } from '@storybook/addon-knobs';
+import DummyContainer from '../_story-helpers/DummyContainer';
 
 import { TagsList, Tag } from 'components/index';
 
@@ -10,12 +11,12 @@ storiesOf('Components/Tag', module)
   .add(
     'TagsList',
     () => (
-      <div style={{width: 500, background: '#f9f9f9', padding: 32}}>
+      <DummyContainer>
         <TagsList className={text('className', 'class1 class2')}>
           <Tag>Simple Tag </Tag>
           <Tag>Simple very big text Tag</Tag>
           <Tag>Simple Tag </Tag>
         </TagsList>
-      </div>
+      </DummyContainer>
     )
   );
---
+++ stories/_story-helpers/DummyContainer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default ({ children }) => (
+  <div style={{width: 500, background: '#f9f9f9', padding: 32}}>
+    {children}
+  </div>
+);
