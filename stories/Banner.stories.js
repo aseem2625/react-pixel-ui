4e04421e1a07d22f6d84102ab16e3f2711f42fe8
aseemgupta <aseemgupta2625@gmail.com>
2019-06-22 03:13:12 +0530

    [Storybook]: Update Banner

--- stories/Banner.stories.js
+++ stories/Banner.stories.js
@@ -1,22 +1,30 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { text, select } from '@storybook/addon-knobs';
+import { text } from '@storybook/addon-knobs';
+import DummyContainer from './_story-helpers/DummyContainer';
 
 import { Banner } from 'components/index';
+import Icon from 'components/Icon/Icon';
 
 
 storiesOf('Components', module)
   .add(
     'Banner',
     () => {
+      const className = text('className', 'class1 class2'),
+        uiClass = text('uiClass', 'ui-hasShadow ui-bg-red ui-txt-white'); // Try ui-bg-green, ui-bg-yellow
+
       return (
-        <Banner
-          className={text('Classes', 'class1 class2')}
-          type={select('Type', ['', 'green', 'red', 'yellow'])}
-        >
-          Add some Banner text
-        </Banner>
+        <DummyContainer>
+          <Banner
+            className={className}
+            uiClass={uiClass}
+          >
+            <Icon name="info" uiClass="ui-svg-size-lg"/>
+            <span>Add some Banner text here and see it wrapping inside this Banner parent.</span>
+          </Banner>
+        </DummyContainer>
       );
     }
   );
