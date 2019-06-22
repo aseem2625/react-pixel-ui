d5809ea7227dc50fc33ed72665ab8c45d60b3fcc
aseemgupta <aseemgupta2625@gmail.com>
2019-06-22 18:07:15 +0530

    [Storybook]: Update HelpText

--- stories/HelpText.stories.js
+++ stories/HelpText.stories.js
@@ -1,21 +1,24 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { boolean } from '@storybook/addon-knobs';
+import { text } from '@storybook/addon-knobs';
+import DummyContainer from './_story-helpers/DummyContainer';
 
 import { HelpText } from 'components/index';
+import Icon from 'components/Icon/Icon';
 
 
 storiesOf('Components', module)
   .add(
     'HelpText',
-    () => {
-      const isLarge = boolean('Large', true);
-
-      return(
-        <HelpText isLarge={isLarge}>
-          This is {isLarge ? 'large' : 'default'} info icon size
+    () => (
+      <DummyContainer>
+        <HelpText
+          uiClass={text('uiClass', 'ui-size-md')}
+        >
+          <Icon name="info" uiClass="ui-svg-size-md" />
+          <span>This is helptext icon. Wrap in span tag for alignment. This is helptext icon. Wrap in span tag for alignment.</span>
         </HelpText>
-      );
-    }
+      </DummyContainer>
+    )
   );
