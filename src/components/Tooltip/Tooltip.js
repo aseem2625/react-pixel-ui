08bcc3615c0481c66c6031758025a9e79da56ce2
aseemgupta <aseemgupta2625@gmail.com>
2019-06-23 17:09:50 +0530

    Improve Tooltip

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -7,35 +7,87 @@ import './Tooltip.styl';
  *
  * */
 export default class Tooltip extends React.Component {
-  state = {};
-  delay = this.props.delay || 0; // default delay = 0ms
-
   constructor(props) {
     super(props);
+    this.state = {};
 
     this.onMouseEnter = this._onMouseEnter.bind(this);
     this.onMouseLeave = this._onMouseLeave.bind(this);
   }
 
   _onMouseEnter(e) {
-    this.setPosition();
+    this.props.onMouseEnter && this.props.onMouseEnter();
   }
 
   // Location aware positioning
-  setPosition() {}
+  shiftPosition() {
+    const tooltip = this.tooltip,
+      tooltipContent = this.tooltipContent;
+
+    const { x, y, width: triggerWidth } = tooltip.getBoundingClientRect(),
+      { width: contentWidth } = tooltipContent.getBoundingClientRect();
+
+    tooltipContent.style.top = y + 'px';
+    tooltipContent.style.left = (x - (contentWidth / 2) + (triggerWidth / 2)) + 'px';
+
+
+    const frameRect = document.body.getBoundingClientRect();
+
+
+    const newBoundingRect = tooltipContent.getBoundingClientRect();
+
+    const netPosToolTip = newBoundingRect.x + newBoundingRect.width;
+    const netPosFrame = frameRect.x + frameRect.width;
+
+
+    if(netPosToolTip > netPosFrame) { // Right side of tooltip exceeding right side of frame
+      // this.set({toolTipPosShift: 'left'});
+      tooltipContent.style.left = (x - contentWidth + triggerWidth ) + 'px';
+    } else if (newBoundingRect.x < frameRect.x) { // Left side of tooltip is falling behind left side of frame
+      // this.set({toolTipPosShift: 'right'});
+      tooltipContent.style.left = x + 'px';
+    }
+  }
+
+  componentDidMount() {
+    this.calcTrueWidth();
+  }
+
+  componentDidUpdate() {
+    this.calcTrueWidth();
+  }
+
+  calcTrueWidth() {
+    const tooltipContent = this.tooltipContent;
+    tooltipContent.style.position = 'fixed';
+    tooltipContent.style.transform = 'scale(1)';
+
+    // Calculate full width as per position: fixed
+    const renderWidthWithFixed =  tooltipContent.getBoundingClientRect().width;
+    tooltipContent.style.width = Math.ceil(renderWidthWithFixed) + 'px';
+
+    // Reset to original styles
+    tooltipContent.style.position = '';
+    tooltipContent.style.transform = '';
+  }
 
   _onMouseLeave(e) {
     this.props.onMouseLeave && this.props.onMouseLeave();
   }
 
+  setRefTooltipContent = e => (this.tooltipContent = e);
+  setRefTooltip = e => (this.tooltip = e);
+
   render() {
-    const { className, children, tooltipText } = this.props;
+    const { className, uiClass, uiClassContent, children, tooltipContent } = this.props;
 
     return (
       <div
+        ref={this.setRefTooltip}
         className={classList(
           'Tooltip',
-          prefixToClasses('Tooltip--', className)
+          prefixToClasses('Tooltip--', className),
+          uiClass
         )}
       >
         <span
@@ -46,7 +98,21 @@ export default class Tooltip extends React.Component {
           {children}
         </span>
 
-        <span className="Tooltip-text">{tooltipText}</span>
+        <SmartOver>
+        </SmartOver>
+
+        <span
+          ref={this.setRefTooltipContent}
+          className={
+            classList(
+              'Tooltip-content',
+              uiClassContent
+            )}
+
+        >
+          {tooltipContent}
+          <span class="arrow"></span>
+        </span>
       </div>
     );
   }
