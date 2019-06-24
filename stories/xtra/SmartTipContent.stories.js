2d54d89abe05574d1058b80c6e1a30f52fd21213
aseemgupta <aseemgupta2625@gmail.com>
2019-06-24 17:02:11 +0530

    [Storybook]: Add SmartTipContent

---
+++ stories/xtra/SmartTipContent.stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { text } from '@storybook/addon-knobs';
+
+import { SmartTipContent } from 'components/index';
+
+storiesOf('Components/Utilities', module)
+  .add(
+    'SmartTipContent',
+    () => {
+      const className = text('className', 'Custom'),
+        uiClass = text('uiClass', 'ui-hasShadow ui-bg-black ui-txt-white'), // Try ui-bg-white
+      children = text(
+        'children',
+        `SmartTipContent adjusts width of tip based content as per content. You can set max-width. Width of SmartTipContent is independent of parent width. Try changing css:position and css:width of parent.`
+      );
+
+      return(
+        <div style={{position: 'absolute', left: 640, top: 60}}>
+          <SmartTipContent
+            className={className}
+            uiClass={uiClass}
+          >
+            {children}
+          </SmartTipContent>
+          Here is actual content
+        </div>
+      );
+    }
+  );
