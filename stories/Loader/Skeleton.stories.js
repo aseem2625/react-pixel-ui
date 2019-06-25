8f90791b192d21e8e74b3f499c958a18772059c7
aseemgupta <aseemgupta2625@gmail.com>
2019-06-25 17:50:16 +0530

    [Storybook]: Update GhostLoader into Skeleton

---
+++ stories/Loader/Skeleton.stories.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { text } from '@storybook/addon-knobs';
+
+import { Skeleton } from 'components/index';
+
+storiesOf('Components/Loader', module)
+  .add(
+    'Skeleton',
+    () => {
+      // Pass skeleton structure upon which shimmering will be applied
+      return (
+        <Skeleton className={text('className', 'Custom')}>
+          <div>
+            <div className="placeholder circle" style={{display: 'inline-block'}}></div>
+            <div className="placeholder" style={{width: 200, display: 'inline-block'}}></div>
+            <div className="placeholder" style={{width: 80, display: 'inline-block'}}></div>
+          </div>
+          <div className="placeholder"></div>
+          <div className="placeholder"></div>
+          <br />
+          <br />
+          <div className="placeholder" style={{height: 32}}></div>
+        </Skeleton>
+      );
+    }
+  );
